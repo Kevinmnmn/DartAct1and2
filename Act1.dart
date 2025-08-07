b2967a4681d18491090f9b4fcecfe28a2149d70b
Kevinmnmn <macolkevin677@gmail.com>
2025-08-07 12:54:32 +0800

    Activity 1 and 2 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,15 @@
+// void main() {
+//   String name = 'Alice';
+//   print("Welcome,"+ name + "!");
+
+// }
+
+
+// void main() {
+//   dynamic name1 = 'Alice';
+//   name1 = 25;
+    
+//   print("Welcome, $name1!");
+// }
+
+// why the dynamic variable causing error because can chance the variable while the program is running and the compiler won't allow changing int to string. 
---
+++ act2.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  double result = calculatePerimeter(10, 5, false);
+    print("The result of rectangle is " + result.toString());
+  
+  result = calculatePerimeter(7, 4, true);
+    print("The result of square is " + result.toString());
+  
+}
+
+double calculatePerimeter(double width, double height, bool isSquare) {
+  double shape;
+  if (isSquare){
+    shape = 4 * width;
+  }else{
+    shape = width * height / 2;
+  }
+  
+  return shape;
+  
+}
